6d6cde6279919ada0c655a141a54831df205b46f
Harmtan13 <Harmtan2@gmail.com>
2018-03-26 15:11:59 -0600

    Finishes Exercises 5-7

---
+++ Ruby/Ruby_Hard_way/ex5.rb
@@ -0,0 +1,17 @@
+# I have replaced my own information instead of using the authors of Learn Ruby the Hard Way
+my_name = 'Tanner B. Harman'
+my_age = 27
+my_height = 67 # inches
+my_weight = 165 # pounds
+my_eyes = 'Hazel'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about my #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not incredibly heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the soda intake."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
