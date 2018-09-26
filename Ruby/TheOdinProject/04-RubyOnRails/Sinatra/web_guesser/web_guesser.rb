31ee3cdfbc4184607295c686d91a904dd7f50fb4
Tanner Harman <harmtan2@gmail.com>
2018-09-25 23:31:04 -0600

    I3: number guessing game complete

--- Ruby/TheOdinProject/04-RubyOnRails/Sinatra/web_guesser/web_guesser.rb
+++ Ruby/TheOdinProject/04-RubyOnRails/Sinatra/web_guesser/web_guesser.rb
@@ -1,8 +1,44 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+set n: rand(100) + 1
 
 get '/' do
-  erb :index, locals: {number: number}
+  guess = params['guess']
+  message = check_guess(guess)
+
+  erb :index, locals: {
+                        number: settings.n, 
+                        guess: guess,
+                        message: message,
+                      }
+end
+
+
+def message(guess)
+  n = settings.n
+  guess = guess.to_i
+
+  to_low = n - 5
+  to_high = n + 5
+  
+  if guess > to_high
+    'Way to High'
+  elsif guess < to_low
+    'Way to Low'
+  elsif guess > n
+    'To High'
+  elsif guess < n
+    'To Low'
+  elsif guess == n
+    'You got it right!'
+  end
+end
+
+def check_guess(guess)
+  if guess == nil
+    "You haven't guessed yet!"
+  else
+    message(guess)
+  end
 end
