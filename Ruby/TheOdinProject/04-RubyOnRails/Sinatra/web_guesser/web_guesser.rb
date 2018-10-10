e210a00c6957f680845e50efc13ac5a94cddec0e
Tanner Harman <harmtan2@gmail.com>
2018-10-10 09:21:49 -0600

    Added Colored Responses & Displayed number when guessed correctly

--- Ruby/TheOdinProject/04-RubyOnRails/Sinatra/web_guesser/web_guesser.rb
+++ Ruby/TheOdinProject/04-RubyOnRails/Sinatra/web_guesser/web_guesser.rb
@@ -1,18 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+@@guess_limit = 10
 set n: rand(100) + 1
 
 get '/' do
   guess = params['guess']
   message = check_guess(guess)
   color = background_color(guess)
+  @@guess_limit -= 1
 
   erb :index, locals: {
                         number: settings.n, 
                         guess: guess,
                         message: message,
                         color: color,
+                        guesses: @@guess_limit,
                       }
 end
 
@@ -33,13 +36,16 @@ def message(guess)
   elsif guess < n
     'To Low'
   elsif guess == n
-    'You got it right!'
+    reset_message(guess)
   end
 end
 
 def check_guess(guess)
   if guess == nil
     "You haven't guessed yet!"
+  elsif @@guess_limit == 0 || guess == settings.n
+    reset_game
+    reset_message(guess)
   else
     message(guess)
   end
@@ -52,9 +58,23 @@ def background_color(guess)
     'red'
   elsif message(guess) == 'To High' || message(guess) == 'To Low'
     'rgba(255,0,0, 0.5)'
-  elsif message(guess) == 'You got it right!'
+  elsif message(guess) == 'You got it right! Game has been Reset!'
     'green'
   else 
     'white'
   end
 end
+
+def reset_message(guess)
+  if @@guess_limit == 0
+    "You've run out of guesses, Game has been reset!"
+  elsif guess == settings.n
+    "You got it right! Game has been Reset!"
+  end
+end
+
+def reset_game
+  @@guess_limit = 10
+  settings.n = rand(100) + 1
+end
+
