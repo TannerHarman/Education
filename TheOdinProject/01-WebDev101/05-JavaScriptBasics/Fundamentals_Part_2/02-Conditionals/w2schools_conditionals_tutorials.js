e6db49875832a2b968789d109e52ac1e9a585295
Tanner Harman <harmtan2@gmail.com>
2019-04-14 22:58:07 -0600

    Added Missed exercies from being silly with Git

---
+++ TheOdinProject/01-WebDev101/05-JavaScriptBasics/Fundamentals_Part_2/02-Conditionals/w2schools_conditionals_tutorials.js
@@ -0,0 +1,21 @@
+// if Statement
+  if (hour < 18 ) {
+    greeting = "Good day";
+  }
+
+// else Statment
+  if (hour < 18 ) {
+    greeting = "Good day";
+  } else {
+    greeting = "Good Evening";
+  }
+
+// else if Statement
+
+  if (hour < 10 ) {
+    greeting = "Good Morning";
+  } else if (time < 20) {
+    greeting = "Good Day";
+  } else {
+    greeting = "Good Evening"
+  }
