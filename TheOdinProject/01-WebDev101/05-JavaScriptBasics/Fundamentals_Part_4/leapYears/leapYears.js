25cfe5fff7ed14882b6823d8d075bc251a96cfaf
Tanner Harman <harmtan2@gmail.com>
2019-05-02 19:34:05 -0600

    Refactored to single line after looking at Odin's solution compared to my own.

--- TheOdinProject/01-WebDev101/05-JavaScriptBasics/Fundamentals_Part_4/leapYears/leapYears.js
+++ TheOdinProject/01-WebDev101/05-JavaScriptBasics/Fundamentals_Part_4/leapYears/leapYears.js
@@ -1,9 +1,6 @@
 const leapYears = function(year) {
-  if ((year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return ((year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0)
 }
 
+console.log(leapYears(1997))
 module.exports = leapYears
