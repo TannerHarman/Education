3e717acf77e2d84c0b5f6687f0ad37fa51e2ee05
Tanner Harman <harmtan2@gmail.com>
2019-05-02 19:14:42 -0600

    Looked at solution and refactored to consolidate code.

--- TheOdinProject/01-WebDev101/05-JavaScriptBasics/Fundamentals_Part_4/sumAll/sumAll.js
+++ TheOdinProject/01-WebDev101/05-JavaScriptBasics/Fundamentals_Part_4/sumAll/sumAll.js
@@ -3,27 +3,14 @@ const sumAll = function(num1, num2) {
   let start = Math.min(num1, num2)
   let end = Math.max(num1, num2)
 
-  let negNumber = function() {
-    if (num1 < 0 || num2 < 0) {
-      return true
-    } else {
-      return false
-    }
-  };
+  let errorMessage = "ERROR"
+ 
+  if (start < 0 || end < 0) return errorMessage;
 
-  let wrongDataType = function() {
-    if (typeof num1 != 'number' || typeof num2 != 'number') {
-      return true
-    } else {
-      return false
-    }
-  }
-  
-  if (negNumber() || wrongDataType()) {
-    return 'ERROR'
-  } else {
-      return (end*(end+1) - (start-1)*start) / 2;
-    };
+  if (typeof num1 != 'number' || typeof num2 != 'number') return errorMessage;
+
+  return (end*(end+1) - (start-1)*start) / 2;
+
 }
-console.log(sumAll(1, 4));
+
 module.exports = sumAll
