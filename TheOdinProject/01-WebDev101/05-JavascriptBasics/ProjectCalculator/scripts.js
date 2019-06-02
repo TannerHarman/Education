4d46a769f4535144bdcd0937910d9d7f32ba6d76
Tanner Harman <harmtan2@gmail.com>
2019-06-01 18:41:49 -0600

    Fully Functional Calculator. Add backspace button & create own math functions instead of using Eval()

--- TheOdinProject/01-WebDev101/05-JavascriptBasics/ProjectCalculator/scripts.js
+++ TheOdinProject/01-WebDev101/05-JavascriptBasics/ProjectCalculator/scripts.js
@@ -1,60 +1,98 @@
-const calculations = document.querySelector('#calculations')
-const equalSymbol = document.querySelector('#equals')
-const total = document.querySelector('#total')
+const calculation = document.querySelector('#calculations');
+const operator = document.querySelector('#equals');
+const total = document.querySelector('#total');
+const clear = document.querySelector('.clear');
 const inputs = document.querySelectorAll('.input');
 const operators = document.querySelectorAll('.operator');
 const equals = document.querySelector('.equal');
 const decimal = document.querySelector('.decimal');
 
-let currentNum = ''; 
-let runningTotal = 0;
+let previousNum = '';
+let currentNum = '';
+let operatorSym = '';
 let equation = [];
+
+let clearEverything = () => {
+  equation = [];
+  currentNum = '';
+  previousNum = '';
+  operatorSym = '';
+  calculations.textContent = '';
+  total.textContent = '';
+  operator.textContent = '';
+}
+
+let updateTotal = (val) => {
+  total.textContent = val;
+}
+
+let updateEquation = () => {
+  calculation.textContent = equation.join('');
+}
 
 function addNum() {
   currentNum += this.textContent;
-  calculations.textContent += this.textContent;
+  updateTotal(currentNum);
 }
-
-function addOperator() {
-  lastChar = equation[equation.length - 1];
-  equation.push(currentNum);
-
-  if (lastChar == '+' || 
-      lastChar == '-' || 
-      lastChar == '×' || 
-      lastChar == '÷') {
-    lastChar = this.textContent;
-    calculations.textContent += this.textContent;
-    currentNum = '';
-  } else {
-    equation.push(this.textContent);
-    calculations.textContent += this.textContent;
-    currentNum = '';
-  }
-};
 
 function addDecimal() {
   if (!currentNum.includes('.')) {
-    currentNum += '.';
+    currentNum += this.textContent;
+  }
+  updateTotal(currentNum);
+}
+
+function addOperator() {
+  equation = [];
+    if (previousNum && currentNum && operatorSym) {
+      let currentSet = [];
+      currentSet.push(previousNum, operatorSym, currentNum);
+      equation.push(currentSet.join(''));
+      
+      calculation.textContent = previousNum = calculate();
+      operator.textContent = operatorSym = this.textContent;
+      total.textContent = currentNum = '';
+  } else if (previousNum) {
+      equation.push(previousNum);
+      operator.textContent = operatorSym = this.textContent;
+      calculation.textContent = previousNum = calculate();
+  } else {
+    operatorSym = this.textContent;
+    previousNum = currentNum;
+    currentNum = '';
+
+    calculation.textContent = previousNum;
+    operator.textContent = operatorSym;
+    total.textContent = '';
   }
 };
 
-function outputArray() {
-  equation.push(currentNum);
-  console.log(equation.join(''));
-  console.log(eval(analyzeEqation(equation)));
-  equation = [];
-  currentNum = '';
-};
-
-function analyzeEqation(equation) {
+function analyzeEqation() {
   newEquation = equation.join('')
   newEquation = newEquation.replace(/×/g, '*');
   newEquation = newEquation.replace(/÷/g, '/');
   return newEquation
 };
 
+let calculate = () => {
+  return runningTotal = eval(analyzeEqation());
+}
+
+const equate = () => {
+  if (previousNum && currentNum && operatorSym) {
+    equation = [];
+    let currentSet = [];
+    currentSet.push(previousNum, operatorSym, currentNum);
+    equation.push(currentSet.join(''));
+    
+    calculation.textContent = previousNum = calculate();
+    operator.textContent = operatorSym = this.textContent;
+    total.textContent = currentNum = '';
+  }
+}
+
 inputs.forEach(number => number.addEventListener('click', addNum));
-operators.forEach(opp => opp.addEventListener('click', addOperator))
 decimal.addEventListener('click', addDecimal);
-equals.addEventListener('click', outputArray);
+operators.forEach(opp => opp.addEventListener('click', addOperator))
+equals.addEventListener('click', equate);
+clear.addEventListener('click', clearEverything);
