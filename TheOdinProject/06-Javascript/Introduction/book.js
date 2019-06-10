86270d4bb2e57c39bcaf39b4e5b41fa0afb35eb7
Tanner Harman <harmtan2@gmail.com>
2019-06-10 13:57:39 -0600

    Started YouTube Clone Project

---
+++ TheOdinProject/06-Javascript/Introduction/book.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function() {
+  return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
+}
+
+let firstBook = new Book('Night Angel Trilogy', 'Brent Weeks', 'Lots of Pages', 'Ready it immensely');
+
+console.log(firstBook.info());
