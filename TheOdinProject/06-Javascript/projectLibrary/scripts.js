1b0c24ee7e10461f4a7e595af8e1ef8c397d82fe
Tanner Harman <harmtan2@gmail.com>
2019-07-17 05:44:55 -0600

    Got basic form built

--- TheOdinProject/06-Javascript/projectLibrary/scripts.js
+++ TheOdinProject/06-Javascript/projectLibrary/scripts.js
@@ -1,4 +1,6 @@
-let myLibrary = [];
+let myLibrary = [
+
+];
 
 function Book() {
   // the constructor...
@@ -7,3 +9,22 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+
+
+/*
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function() {
+  return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
+}
+
+let firstBook = new Book('Night Angel Trilogy', 'Brent Weeks', 'Lots of Pages', 'Ready it immensely');
+
+console.log(firstBook.info());
+*/
